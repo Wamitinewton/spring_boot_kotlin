e74fe22df464c00f0da328b145e0c3f6628a1b31
newton <wamitinewton@gmail.com>
2025-04-01 00:37:33 -0400

    init prject

---
+++ src/test/kotlin/com/newton/spring_boot_kotlin/SpringBootKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.newton.spring_boot_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
